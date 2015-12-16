0e054afe27220ba8c84a6d081b7c07f44a93f823
Adam Holsinger <adamdude828@gmail.com>
2015-12-16 08:00:25 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# localmake
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/localmake/metadata.rb
@@ -0,0 +1,7 @@
+name 'localmake'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures localmake'
+long_description 'Installs/Configures localmake'
+version '0.1.0'
